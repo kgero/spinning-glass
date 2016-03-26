f03172f6f71f9db7daa4c387ecc21a4f54701d8a
Katy Gero <katystreet@gmail.com>
2016-03-26 14:14:52 -0400

    fixed problems in amendglassinfo and find_speed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import src.glass_slides as glass_slides
 import src.water_shape as water_shape
 import src.glass_tips as glass_tips
 
-
+import math
 
 
 glass_info = {
@@ -32,19 +32,31 @@ def amend_glass_info(glass_info):
 	 	True
 	 '''
 	# New calculations
-	 inside_glass_vol = (1/3)*pi*(b_radius**2 + b_radius*t_radius + t_radius**2)*height
-	 outside_glass_vol = (1/3)*pi*((b_radius+thickness)**2
+	height = glass_info['height']
+	b_radius = glass_info['b_radius']
+	t_radius = glass_info['t_radius']
+	thickness = glass_info['thickness']
+	percent_full = glass_info['percent_full']
+	density = glass_info['density']
+	c_f = glass_info['c_f']
+	pi = math.pi
+
+	inside_glass_vol = (1/3.)*pi*(b_radius**2 + b_radius*t_radius + t_radius**2)*height
+	outside_glass_vol = (1/3.)*pi*((b_radius+thickness)**2
 	 					+ (b_radius+thickness)*(t_radius+thickness) 
 	 					+ (t_radius+thickness)**2)*height
-	 glass_vol = outside_glass_vol - inside_glass_vol 	# m^3
-	 water_vol = inside_glass_vol * percent_full 		# m^3
-	 mass_glass = density * glass_vol 					# kg
-	 mass_water = 1000 * water_vol 						# kg
-	 total_mass = mass_glass + mass_water				# kg
+	glass_vol = outside_glass_vol - inside_glass_vol 	# m^3
+	water_vol = inside_glass_vol * percent_full 		# m^3
+	mass_glass = density * glass_vol 					# kg
+	mass_water = 1000 * water_vol 						# kg
+	total_mass = mass_glass + mass_water				# kg
 
-	 # Amend Values
-	 glass_info['total_mass'] = total_mass
-	 glass_info['mass_water'] = mass_water
+	# Amend Values
+	glass_info['total_mass'] = total_mass
+	glass_info['mass_water'] = mass_water
+	glass_info['r_0'] =  glass_info['start_radius'] - glass_info['b_radius']
+	glass_info['r_1'] =  glass_info['start_radius'] + glass_info['b_radius']
+	glass_info['alpha'] = 2*glass_info['b_radius']/glass_info['start_radius']  # check this
 	return True
 
 
@@ -62,11 +74,11 @@ def run_simulation(glass_info, speed):
 	 			1 - event occured
 	 '''
 
-	 slides = glass_slides.main(glass_info, speed)
-	 brim, delta_water_cg = water_shape.main(glass_info, speed)
-	 tips = glass_tips.main(glass_info, delta_water_cg, speed)
+	slides = glass_slides.main(glass_info, speed)
+	brim, delta_water_cg = water_shape.main(glass_info, speed)
+	tips = glass_tips.main(glass_info, delta_water_cg, speed)
 
-	 return (slides, brim, tips)
+	return (slides, brim, tips)
 
 
 def find_speed(interval=1):
@@ -87,20 +99,21 @@ def find_speed(interval=1):
 	max_speed = 100
 	event_occurred = False
 
-	while not event_occurred or speed <= max_speed:
+	while not event_occurred:
+		if speed >= max_speed:
+			raise ValueError('No event occurred. Something went wrong?')
 		print "Running simulation at", speed, "rad/s..."
 		curr_result = run_simulation(glass_info, speed)
 		if sum(curr_result) == 1:
 			event_occurred = True
+			print 'break!'
 		elif sum(curr_result) > 1:
 			raise ValueError('More than one event occurred! Try decreasing your interval.')
 		speed += interval
 
-	if event_occurred:
-		return (speed, curr_result.index(1)+1)
-	else:
-		raise ValueError('No event occurred. Something went wrong?')
+	return (speed, curr_result.index(1)+1)
+		
 
 if __name__ == "__main__":
-	amend_glass_info()
+	amend_glass_info(glass_info)
 	print find_speed()
