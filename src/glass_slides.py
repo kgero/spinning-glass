09fef4793b0521596f502364c37a28bd2585ab46
Silas Hughes <silas@restdevices.com>
2016-03-26 13:40:01 -0400

    added amend_glass_info, simplified glass_slides.py

--- src/glass_slides.py
+++ src/glass_slides.py
@@ -17,21 +17,10 @@ def main(glass_info,speed):
 	 			1: if glass does slide
 	 '''
 
-	 # Calulate the mass of the glass and water
-	 inside_glass_vol = (1/3)*pi*(b_radius**2 + b_radius*t_radius + t_radius**2)*height
-	 outside_glass_vol = (1/3)*pi*((b_radius+thickness)**2
-	 					+ (b_radius+thickness)*(t_radius+thickness) 
-	 					+ (t_radius+thickness)**2)*height
-	 glass_vol = outside_glass_vol - inside_glass_vol 	# m^3
-	 water_vol = inside_glass_vol * percent_full 		# m^3
-	 mass_glass = density * glass_vol 					# kg
-	 mass_water = 1000 * water_vol 						# kg
-	 total_mass = mass_glass + mass_water
-
 	 # Calculate the centrifugal force at the give speed
 	 # M * w^2 * r
-	 centrifugal_force = total_mass * (speed**2) * start_radius # N
-	 normal_force = total_mass * 9.8							# N
+	 centrifugal_force = glass_info['total_mass'] * (speed**2) * glass_info['start_radius']
+	 normal_force = glass_info['total_mass'] * 9.8
 
 	 if centrifugal_force > normal_force * c_f:
 	 	return 1
